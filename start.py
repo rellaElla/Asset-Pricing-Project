c6ddb3599d68ea0be5d81fe78c706aff8634ea57
Alex Parella <apar20@bu.edu>
2020-06-15 19:31:04 -0400

    First Commit

---
+++ start.py
@@ -0,0 +1,14 @@
+def pull_data():
+    # TODO
+    return 0,0
+
+def main():
+    print("main")
+    # pull training/testing data
+    training, testing = pull_data()
+    # train model
+
+
+
+if(__name__ == "__main__"):
+    main()
