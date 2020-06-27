231580c1cc57743e41feda564509628590d3d16c
Alex Parella <apar20@bu.edu>
2020-06-26 21:20:29 -0400

    Pulling tweets for  and storing into .xls file for data analysis

--- start.py
+++ start.py
@@ -1,40 +1,59 @@
 import tweepy
 import os
+import time
+import xlwt
+from xlwt import Workbook
 
 consumer_key = os.environ['twitter_consumer_key']
 consumer_secret = os.environ['twitter_consumer_secret']
 access_key = os.environ['twitter_access_key']
 access_secret = os.environ['twitter_access_secret']
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-def get_tweets(username):
+def get_tweets(ticker, count = 1000):
 
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_key, access_secret)
-        api = tweepy.API(auth)
+    ret = []
+    tweets = []
 
-        tweets = api.user_timeline(screen_name=username, count = 1)
+    q=str(ticker)
+    fetched_tweets = api.search(q, count = count, lang="en")
 
-        tmp=[]
+    for tweet in fetched_tweets:
+        if(tweet.text not in tweets):
+            tweets.append(tweet.text)
+            t = (ticker, tweet.created_at, tweet.user.screen_name, tweet.text)
+            ret.append(t)
 
-        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created
-
-        for j in tweets_for_csv:
-            tmp.append(j)
-            #print(j+"\n")
-        return tmp
-
+    return ret
 
 def main():
-    # TO-DO: format tweets - get_tweets should return an array of tuples
-    # composed of (stock ticker, tweet, date of tweet, user)
-    # store each tuple in a csv
-    get_tweets("realDonaldTrump")
-    get_tweets("wallstreetbets")
-    get_tweets("Benzinga")
-    get_tweets("Stocktwits")
-    get_tweets("CNBC")
 
+    ticker = "$XOM"
+    tweets = get_tweets(ticker)
+    l = len(tweets)
+
+    wb = Workbook()
+
+    # add_sheet is used to create sheet.
+    sheet1 = wb.add_sheet('Sheet 1')
+    sheet1.write(0, 0, 'Ticker')
+    sheet1.write(0, 1, 'Date of Tweet')
+    sheet1.write(0, 2, 'User')
+    sheet1.write(0, 3, 'Tweet Content')
+
+    j = 1
+    for i in tweets:
+        sheet1.write(j, 0, i[0])
+        sheet1.write(j, 1, i[1])
+        sheet1.write(j, 2, i[2])
+        sheet1.write(j, 3, i[3])
+        print(i[1])
+        j+=1
+
+    wb.save('output.xls')
 
 
 if(__name__ == "__main__"):
